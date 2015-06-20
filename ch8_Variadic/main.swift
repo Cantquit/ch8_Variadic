22476a0787b43258a3905010808ede25f7f7f482
Nicolas Ducat <nicolas.ducat@gmx.net>
2015-06-20 12:20:52 +0200

    Initial Commit

---
+++ ch8_Variadic/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ch8_Variadic
+//
+//  Created by Nicolas on 20.06.15.
+//  Copyright (c) 2015 Peter Pan Enterprises. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
